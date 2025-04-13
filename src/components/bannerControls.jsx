15019b639b6bd7087c95b7a3b778ad8d49ce25ab
preshgit <preciousdesk10@gmail.com>
2025-04-13 19:47:49 +0100

    enhancement: Edit background color in bannerControls component.
    `BannerControls.jsx` contains the form used to manipulate  the banner content.

--- src/components/bannerControls.jsx
+++ src/components/bannerControls.jsx
@@ -54,7 +54,7 @@ const BannerControls = ({ bannerConfig, updateBannerConfig }) => {
                 type="color"
                 id="backgroundColor"
                 className="h-10 w-full hover:cursor-pointer md:hover:cursor-pointer rounded-sm"
-                value={bannerConfig.backgroundColor}
+                value={bannerConfig.backgroundColor || "#2563eb"}
                 onChange={(e) =>
                   handleChange("backgroundColor", e.target.value)
                 }
