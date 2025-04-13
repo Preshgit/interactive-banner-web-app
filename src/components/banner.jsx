44584450d05366cddb3b3fe349f918be9bb2d588
preshgit <preciousdesk10@gmail.com>
2025-04-13 18:52:50 +0100

    banner: Edit style in Banner and BannerControl component
    `BannerControl.jsx` contains the form used to manipulate  the banner content.

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -13,7 +13,7 @@ const Banner = ({ bannerConfig }) => {
     <div className="flex items-center max-w-screen mb-8 shadow-lg rounded-lg overflow-hidden">
       <div
         style={{ backgroundColor }}
-        className="p-6 sm:p-10 bg-[#2563eb] max-w-screen"
+        className="p-6 sm:p-10 bg-[#2563eb] w-full rounded-lg"
       >
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="w-full md:w-1/2 text-white">
